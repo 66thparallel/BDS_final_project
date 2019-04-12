d6cf62d541ac63ae344babc7d9ed02e818d764de
mengli0307 <43049679+mengli0307@users.noreply.github.com>
2019-04-12 13:25:46 -0400

    Add files via upload

---
+++ bdsproject_merge.py
@@ -0,0 +1,14 @@
+
+# coding: utf-8
+
+# In[17]:
+
+import pandas as pd
+data = pd.read_csv('metadata.txt', sep="\t", header=None)
+data.columns = ["userID","b", "rating", "lable", "date"]
+data=data.drop(['b', 'date'],axis=1)
+data2 = pd.read_csv('reviewContent.txt', sep="\t", header=None)
+data2.columns = ["userID","b", "date", "content"]
+result=data.set_index('userID').join(data2.set_index('userID'))
+result.columns = [ "rating", "lable", "prob_ID","date","content"]
+
