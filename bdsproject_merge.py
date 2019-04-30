1d5311944ec185dea134935d9f9c92fb55367409
Jane Liu <janeikeliu@gmail.com>
2019-04-30 03:35:05 -0400

    Move preprocessing code into main.py. bdsproject_merge.py just contains code to merge data files. Renamed batch processing file to final_project.s

--- bdsproject_merge.py
+++ bdsproject_merge.py
@@ -11,33 +11,17 @@ Classes:
 
 import pandas as pd
 import numpy as np
-import nltk
-from preprocessor import *
 
 
-def main():
-    # data = pd.read_csv('src/metadata.txt', sep="\t", header=None)
-    # data.columns = ["userID","b", "rating", "label", "date"]
-    # data = data.drop(['b', 'date'],axis=1)
-    # data2 = pd.read_csv('src/reviewContent.txt', sep="\t", header=None)
-    # data2.columns = ["userID","b", "date", "content"]
-    # result = data.set_index('userID').join(data2.set_index('userID'))
-    # result.columns = [ "rating", "label", "prob_ID","date","content"]
-    # train, validate, test = np.split(result.sample(frac=1), [int(.6*len(result)), int(.8*len(result))])
+def bdsproject_merge():
 
-    # preprocessing:
-    Prep = Preprocessor()
-    prepped_text = Prep.preprocess()
+    data = pd.read_csv('src/metadata.txt', sep="\t", header=None)
+    data.columns = ["userID","b", "rating", "label", "date"]
+    data = data.drop(['b', 'date'],axis=1)
+    data2 = pd.read_csv('src/reviewContent.txt', sep="\t", header=None)
+    data2.columns = ["userID","b", "date", "content"]
+    result = data.set_index('userID').join(data2.set_index('userID'))
+    result.columns = [ "rating", "label", "prob_ID","date","content"]
+    train, validate, test = np.split(result.sample(frac=1), [int(.6*len(result)), int(.8*len(result))])
 
-    # Find unigrams
-    unigrams = Unigrams(prepped_text)
-    top_unigrams = unigrams.get_top_unigrams()
-
-    # Find bigrams
-    bigrams = Bigrams(prepped_text)
-    top_bigrams = bigrams.get_top_bigrams()
-
-    ngram_print(top_unigrams, top_bigrams)
-
-
-main()
+bdsproject_merge()
