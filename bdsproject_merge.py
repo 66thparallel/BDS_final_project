a6f13e58edb961a9e130922195cf183adabae967
mengli0307 <43049679+mengli0307@users.noreply.github.com>
2019-05-01 11:27:12 -0400

    Update bdsproject_merge.py

--- bdsproject_merge.py
+++ bdsproject_merge.py
@@ -12,16 +12,36 @@ Classes:
 import pandas as pd
 import numpy as np
 
+class dataset:
+    def __init__(self, txt1,txt2):
+        self._txt1 = txt1
+        self._txt2 = txt2
 
-def bdsproject_merge():
 
-    data = pd.read_csv('src/metadata.txt', sep="\t", header=None)
-    data.columns = ["userID","b", "rating", "label", "date"]
-    data = data.drop(['b', 'date'],axis=1)
-    data2 = pd.read_csv('src/reviewContent.txt', sep="\t", header=None)
-    data2.columns = ["userID","b", "date", "content"]
-    result = data.set_index('userID').join(data2.set_index('userID'))
-    result.columns = [ "rating", "label", "prob_ID","date","content"]
-    train, validate, test = np.split(result.sample(frac=1), [int(.6*len(result)), int(.8*len(result))])
+    def bdsproject_merge(self):
+        #merge dataset by userID
+        data = pd.read_csv(self._txt1, sep="\t", header=None)
+        data.columns = ["userID","b", "rating", "lable", "date"]
+        data=data.drop(['b', 'date'],axis=1)
+        data2 = pd.read_csv(self._txt2, sep="\t", header=None)
+        data2.columns = ["userID","b", "date", "content"]
+        result=data.set_index('userID').join(data2.set_index('userID'))
+        
+        #rename column
+        result.columns = [ "rating", "label", "prob_ID","date","content"]
+        
+        #make the dataset balanced
+        fakedata1=result.loc[result["label"]==-1]
+        nfakedata1=result.loc[result["label"]==1]
+        nfakerdata2=nfakedata1.sample(n=105000)
+        result2=pd.concat([fakedata1,nfakerdata2],ignore_index=True)
+        
+        #split dataset to train, validate and test and save it as csv.
+        train, validate, test = np.split(result2.sample(frac=1), [int(.6*len(result2)), int(.8*len(result2))])
+        train.to_csv("train2.csv")
+        validate.to_csv("validate2.csv")
+        test.to_csv("test2.csv")
 
-bdsproject_merge()
+        
+
+
